dc7b9caacee37b7de1c4bd892ef6dc7bcdac1d28
Nitish-Ram <105485958+Nitish-Ram@users.noreply.github.com>
2026-01-05 00:09:21 +0300

    Buy me coffee.

---
+++ html2geo/geoextract.py
@@ -0,0 +1,28 @@
+import sys
+import re
+from geopy.geocoders import Nominatim
+
+print("for my reference, paste then use ctrl + z")
+
+html = sys.stdin.read()
+pattern = r'(-?\d{1,2}\.\d+)\s*,\s*(-?\d{1,3}\.\d+)'
+matches = re.findall(pattern, html)
+
+valid_coords = [(float(lat), float(lon)) for lat, lon in matches if -90<=float(lat)<=90 and -180<=float(lon)<=180]
+if not valid_coords:
+    print("coords lowk trippin")
+    sys.exit(1)
+
+lat, long = valid_coords[0]
+print(f"Latitude: {lat}")
+print(f"Longitude: {long}")
+geolocator = Nominatim(user_agent="html_location_lookup")
+location = geolocator.reverse((lat, long), exactly_one=True, timeout=10)
+if not location:
+    print("couldnt find it buddy")
+    sys.exit(1)
+addr = location.raw.get("address", {})
+
+print("City:", addr.get("city") or addr.get("town") or addr.get("village"))
+print("State:", addr.get("state"))
+print("Country:", addr.get("country"))
---
+++ html2geo/readme.txt
@@ -0,0 +1,8 @@
+For imbeciles that cannot win at geoguesser/openguessr; (Haven't been tested on geoguessr, I'm only assuming.)
+
+1. Press F12 in your geoguessr/openguessr tab.
+2. Go to the network tab.
+3. Filter:
+        Geoguessr --> "GeoPhotoService"
+        Openguessr --> "streetview?location"
+4. Paste the HTML code here. At the end press Ctrl + Z and enter
